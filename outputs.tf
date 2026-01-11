7ed040055beadff5a9e87310de3c5406f5937bf8
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:59:23 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "resource_group_policy_exemptions" {
+  description = "All resource_group_policy_exemption resources"
+  value       = azurerm_resource_group_policy_exemption.resource_group_policy_exemptions
+}
+
